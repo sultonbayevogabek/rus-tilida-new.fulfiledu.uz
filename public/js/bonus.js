709c4cca58659e3583f8840e93744d90b38b10ba
sultonbayevogabek <sultonbayevogabek@gmail.com>
2024-12-18 19:04:26 +0500

    commit

--- public/js/bonus.js
+++ public/js/bonus.js
@@ -90,7 +90,7 @@ try {
       formData.append('Telefon raqami', "+998" + phone);
       formData.append(`Ro'yxatdan o'tgan vaqti`, new Date().toLocaleString());
 
-      location.href = `https://t.me/+WG3VxiSKX2YyYzUy`;
+      location.href = `https://t.me/+psV4u8FsU0BkZjNi`;
 
       submitButton.removeAttribute('disabled');
       submitButton.textContent = "Ro'yxatdan o'tish";
