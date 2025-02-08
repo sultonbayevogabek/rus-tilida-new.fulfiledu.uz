1ce4f9e0cd98b8f7eeb2275f0a6fcc8195f2e044
farruhzoirov <fzoirov29@gmail.com>
2025-02-08 22:34:39 +0500

    commit

--- public/js/script.js
+++ public/js/script.js
@@ -94,7 +94,7 @@ try {
       submitButton.removeAttribute('disabled');
       submitButton.textContent = "Ro'yxatdan o'tish";
       closeModal();
-      window.location.href = `../telegram-old.html`
+      window.location.href = `../telegram.html`
     }
   })
 
